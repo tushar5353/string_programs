525fa2f64f7dd3e87ae327e1d3a4779a8ee219b0
tushar sharma <Tushar@tushars-MacBook-Air.local>
2016-07-24 23:42:16 +0530

    added one more argument during instantiation and a new function for checking permutations in strings

--- string_ques.py
+++ string_ques.py
@@ -5,18 +5,34 @@ class file for containing various string functions
 
 class StringFunctions:
 
-    def __init__(self, str):
-        self.str = str
+    def __init__(self, str1, str2):
+        self.str1 = str1
+        self.str2 = str2
 
     def hasUniqueChars(self):
-        if len(self.str) > 128:
+        """
+        function to determine if the string is having all the characters as 
+        unique or not.
+        """
+        if len(self.str1) > 128:
             return False
 
         total = [False] * 128
 
-        for i in self.str:
+        for i in self.str1:
             if total[ord(i)] is False:
                 total[ord(i)] = True
             else:
                 return False
         return True
+
+
+    def stringPermutation(self):
+        """
+        function to determine if two string are in permutation or not.
+        """
+        if len(self.str1) != len(self.str2):
+            return False
+        else:
+            return "".join(sorted(self.str1)) == "".join(sorted(self.str2))
+
