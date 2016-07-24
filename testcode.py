c56e53f56d5c7fa8543fd61cb09f9d5049901b0f
tushar sharma <Tushar@tushars-MacBook-Air.local>
2016-07-24 23:43:34 +0530

    changed code for testing

--- testcode.py
+++ testcode.py
@@ -1,5 +1,12 @@
 import string_ques
 
-str = "asdf"
-str_func_obj = string_ques.StringFunctions(str)
+str1 = "asdf"
+str2 = "asodf"
+
+str_func_obj = string_ques.StringFunctions(str1, str2)
+
+print("Test for unique Characters in a string:")
 print(str_func_obj.hasUniqueChars())
+
+print("Test for permutation in two strings:")
+print(str_func_obj.stringPermutation())
