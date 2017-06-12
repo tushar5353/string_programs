235651627acbf864ceb381dd6fde58efe373e4d1
tushar5353 <tushar5353@gmail.com>
2017-06-12 20:50:11 +0530

    decoding of digit sequence - only question

---
+++ decoding_of_digit_sequence.py
@@ -0,0 +1,16 @@
+"""
+Count Possible Decodings of a given Digit Sequence
+
+Let 1 represent ‘A’, 2 represents ‘B’, etc. Given a digit sequence, count the number of possible decodings of the given digit sequence.
+
+Examples:
+
+Input:  digits[] = "121"
+Output: 3
+// The possible decodings are "ABA", "AU", "LA"
+
+Input: digits[] = "1234"
+Output: 3
+// The possible decodings are "ABCD", "LCD", "AWD"
+
+"""
